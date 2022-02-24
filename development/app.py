db1c8b232b7b39ba6af3b415e0d366547e2f7eee
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-02-23 22:35:23 -0800

    Define route /register. GET:renders register.html. POST: takes user input and adds user to database

--- development/app.py
+++ development/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from users import User
 
 app = Flask(__name__)
 
@@ -6,6 +7,23 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
 
+        user = User(first_name, last_name, email, username, password)
+        user.register_user()
+
+        return redirect(url_for('home'))
+        
+    else:
+        return render_template('register.html')
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
