6856fc8381c39396f95ee11b05b004f2b69ed277
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-02-24 20:46:35 -0800

    Defined login route and function. Compares if username and password input exist in database. If true redirects user to Dashboard. Otherwise refresh Login page and display "Username/password incorrect"

--- development/app.py
+++ development/app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request, redirect, url_for
 from users import User
 
@@ -16,14 +17,33 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        user = User(first_name, last_name, email, username, password)
-        user.register_user()
+        user = User()
+        user.register_user(first_name, last_name, email, username, password)
 
         return redirect(url_for('home'))
-        
     else:
         return render_template('register.html')
 
-    
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User()
+        #Define boolean login
+        login = user.login_user(username, password)
+
+        if  login is True:
+            #If username and password match, redirect user to his/her dashboard
+            return render_template('/dashboard')
+        else:
+            #If username or password is incorrect, redirect to login page and display "Username/password incorrect"
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
