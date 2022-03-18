ab8354273d8ea317c1bb1ed49698d53f16c98145
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2022-03-17 22:40:54 -0700

    Import connection from database.py. Define instance variable self.connection = connection

--- development/users.py
+++ development/users.py
@@ -1,22 +1,16 @@
-import sqlalchemy
-import pymysql
+from database import connection
 
 class User():
     def __init__(self):
-        pass
+        self.connection = connection
 
     def user_count(self):
         '''Get number of rows in users table'''
-        #syntax: engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE")
-        engine = sqlalchemy.create_engine("mysql+pymysql://root:Jams2009Charlie2014!@localhost/spoti-tours")
-
         #Query to add Id column 
         query = '''SELECT count(*) FROM users;'''
 
-        #Create sql connection
-        connection = engine.connect()
         #Execute query
-        result = connection.execute(query)
+        result = self.connection.execute(query)
 
         #Fetch count value
         user_count = result.fetchall()[0][0]
@@ -25,33 +19,21 @@ class User():
 
         
     def register_user(self, first_name, last_name, email, username, password):
-        '''Add new user to database if user does not exist'''
-        #syntax: engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE")
-        engine = sqlalchemy.create_engine("mysql+pymysql://root:Jams2009Charlie2014!@localhost/spoti-tours")
-
-        #Create sql connection
-        connection = engine.connect()
-        
+        '''Add new user to database if user does not exist'''      
         #Check if this is the first user. If so, id = 1
         if self.user_count() == 0:
             id = 1
             query = '''INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s);'''
             #Execute query
-            connection.execute(query, [id, first_name, last_name, email, username, password])
+            self.connection.execute(query, [id, first_name, last_name, email, username, password])
         else:
             query = '''INSERT INTO users (first_name, last_name, email, username, password) VALUES (%s, %s, %s, %s, %s);'''
             #Execute query
-            connection.execute(query, [first_name, last_name, email, username, password])
+            self.connection.execute(query, [first_name, last_name, email, username, password])
 
 
     def user_exists(self, username, password):
         '''Verifies if username and password match values from database. Returns True or False'''
-        #syntax: engine = create_engine("mysql://USER:PASSWORD@HOST/DATABASE")
-        engine = sqlalchemy.create_engine("mysql+pymysql://root:Jams2009Charlie2014!@localhost/spoti-tours")
-
-        #Create sql connection
-        connection = engine.connect()
-
         #Query to get verify username, password and count. user_exist = 0 or 1
         query = '''SELECT 
             username, 
@@ -59,7 +41,7 @@ class User():
             count(*) as user_exists
             from users
                 where username = %s and password = %s;'''
-        result = connection.execute(query, [username, password])
+        result = self.connection.execute(query, [username, password])
 
         #Fetch count for username and pasword entered. If count is 0, user does not exist
         user_exists = result.fetchall()[0][2]
